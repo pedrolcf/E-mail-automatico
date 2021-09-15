041adda68865f4545dd0682afce5b25e79462d08
João Pedro Fernandes <joaopedrofclol@gmail.com>
2021-09-14 23:48:52 -0300

    Update bancodedados.py

--- script/bancodedados.py
+++ script/bancodedados.py
@@ -2,7 +2,9 @@ import mysql.connector
 usuario = input('Usuário do MySQL:')
 senha = input('Senha MySQL:')
 con = mysql.connector.connect(host = 'localhost' , database = 'cadastro' , user = usuario , password  = senha)
+#isso serve para conectar com o mysql
 
 if con.is_connected():
     db_info = con.get_server_info()
     print(f'está conectado ao Servidor MySQL versão {db_info}')
+#isso serve para mostrar informações no mysql
